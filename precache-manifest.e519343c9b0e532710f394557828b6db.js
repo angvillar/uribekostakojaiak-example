4acbce009d97aa64656b4be96e7839dfa163ba73
angvillar <angvillar@gmail.com>
2019-04-17 15:39:59 +0200

    Updates

---
+++ precache-manifest.e519343c9b0e532710f394557828b6db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29a4b826f823d92515a0",
+    "url": "/uribekostakojaiak-example/static/js/runtime~main.931e01c2.js"
+  },
+  {
+    "revision": "22ed64c7f12a59dfda2e",
+    "url": "/uribekostakojaiak-example/static/js/main.0b5e8526.chunk.js"
+  },
+  {
+    "revision": "8d4b4fc635cba592b6a8",
+    "url": "/uribekostakojaiak-example/static/js/2.5d3351c7.chunk.js"
+  },
+  {
+    "revision": "22ed64c7f12a59dfda2e",
+    "url": "/uribekostakojaiak-example/static/css/main.6e264c67.chunk.css"
+  },
+  {
+    "revision": "e43d966d2e4cf18d382a2a3059ff06ee",
+    "url": "/uribekostakojaiak-example/index.html"
+  }
+];
